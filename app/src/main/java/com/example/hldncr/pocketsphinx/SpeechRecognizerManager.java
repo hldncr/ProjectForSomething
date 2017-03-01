c401ad20b62bd99fc4bfb564b4e8421d2353e09b
hldncr <hld.dncr@gmail.com>
2017-03-01 12:25:53 +0100

    So Many Things I Changed

--- app/src/main/java/com/example/hldncr/pocketsphinx/SpeechRecognizerManager.java
+++ app/src/main/java/com/example/hldncr/pocketsphinx/SpeechRecognizerManager.java
@@ -21,44 +21,50 @@ import edu.cmu.pocketsphinx.SpeechRecognizerSetup;
 public class SpeechRecognizerManager {
 
     private static final String LOG_TAG = "1453" ;
-
     private static final String KWS_SEARCH = "wakeup";
+
+    // This string is setting KeyWord so when Listener recognizer this word , Listener will start the function in MainActivity
     private static final String KEYPHRASE = "help me" ;
+
+    // Open-Source Speech Recognizer
     private edu.cmu.pocketsphinx.SpeechRecognizer mPocketSphinxRecognizer;
+
+    // Context and Communicate Object For Synchronization With MainActivity
     private Context mContext;
     private Communicate com ;
 
 
+    //Constructor Function
     public SpeechRecognizerManager(Context context) {
         this.mContext = context;
         initPockerSphinx();
     }
 
+    // Setting Up and Initialize Speech Recognizer
     private void initPockerSphinx() {
 
         new AsyncTask<Void, Void, Exception>() {
             @Override
             protected Exception doInBackground(Void... params) {
                 try {
+
                     Assets assets = new Assets(mContext);
-
-                    //Performs the synchronization of assets in the application and external storage
                     File assetDir = assets.syncAssets();
-
 
                     //Creates a new speech recognizer builder with default configuration
                     SpeechRecognizerSetup speechRecognizerSetup = SpeechRecognizerSetup.defaultSetup();
 
+                    // For Language
                     speechRecognizerSetup.setAcousticModel(new File(assetDir, "en-us-ptm"));
                     speechRecognizerSetup.setDictionary(new File(assetDir, "cmudict-en-us.dict"));
 
-                    // Threshold to tune for keyphrase to balance between false alarms and misses
-                    speechRecognizerSetup.setKeywordThreshold(1e-15f);
+                    // For Sensivity
+                    speechRecognizerSetup.setKeywordThreshold(1e-25f);
 
-                    //Creates a new SpeechRecognizer object based on previous set up.
+                    //Creates a new SpeechRecognizer object
                     mPocketSphinxRecognizer = speechRecognizerSetup.getRecognizer();
 
-                    // Create keyword-activation search.
+                    // Add KEYWORD and Listener
                     mPocketSphinxRecognizer.addKeyphraseSearch(KWS_SEARCH, KEYPHRASE);
                     mPocketSphinxRecognizer.addListener(new PocketSphinxRecognitionListener());
                 } catch (IOException e) {
@@ -81,25 +87,25 @@ public class SpeechRecognizerManager {
 
 
 
-    // TODO Say Something Here
+    // Setting Communicate Object
     public void setCom(Communicate com) {
         this.com = com;
     }
 
 
-    /** Restart function for loop */
+    // Restart function for loop
     private void restartSearch(String searchName) {
         mPocketSphinxRecognizer.stop();
         mPocketSphinxRecognizer.startListening(searchName);
     }
 
 
-    /** This interface for communicate from SpeechRecognizerManager to MainActivity */
+    // This interface for communicate from SpeechRecognizerManager to MainActivity
     public interface Communicate {
         public void comResult() ;
     }
 
-    //Speech Listener
+    //Speech Listener Object
     protected class PocketSphinxRecognitionListener implements edu.cmu.pocketsphinx.RecognitionListener {
 
         @Override
